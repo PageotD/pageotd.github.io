b8c83757fca5af504b58b61200209b73eabee349
PageotD <damien.pageot@gmail.com>
2024-02-03 11:51:10 +0100

    Improve generate.py; add gitignore

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,42 +1,23 @@
 import json
 from jinja2 import Environment, FileSystemLoader
 
-# # Load data from a JSON file
-# data = {
-#     "title": "About me",
-#     "paragraphs": [
-#         "With a master degree in Planetary Geosciences and a PhD in Geophysics, I had the opportunity to work on several topics and at different scales: lithosphere imaging, sea wall monitoring, concrete monitoring, seismic modeling at reduced scale.",
-#         "After several years spent in research laboratories, I started a reconversion in the IT world by following a DevOps training. This allowed me to join Capgemini and to touch many technologies and methods: Jenkins, Docker, Kubernetes, BIG-IP F5, LDAP as Code..."
-#     ]
-# }
+def  add_data(filename: str)->dict:
+    with open(filename, "r") as fjson:
+        content = json.load(fjson)
+    return {filename.removesuffix(".json") : content}
 
 data = {}
-
-with open("education.json", "r") as feduc:
-    education = json.load(feduc)
-    data.update({"education": education})
-
-with open("experience.json", "r") as fexpe:
-    experience = json.load(fexpe)
-    data.update({"experience": experience})
-
-with open("references.json", "r") as fref:
-    references = json.load(fref)
-    data.update({"references": references})
-
-with open("social.json", "r") as fsoc:
-    social = json.load(fsoc)
-    data.update({"social": social})
+data.update(add_data("social.json"))
+data.update(add_data("education.json"))
+data.update(add_data("experience.json"))
+data.update(add_data("references.json"))
 
 # Set up Jinja environment
 env = Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('index.j2')
 
 # Render the template with the data
-#output = template.render(data)
 output = template.render(data)
 
 with open("../index.html", "w") as findex:
     findex.write(output)
-# Print or save the rendered HTML
-#print(output)
