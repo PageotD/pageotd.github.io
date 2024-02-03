817b32673d1c4e1c3caa495424d1a4051dd6d9ee
PageotD <damien.pageot@gmail.com>
2024-02-03 15:16:07 +0100

    Interests section + interests.json

--- scripts/generate.py
+++ scripts/generate.py
@@ -11,6 +11,7 @@ data.update(add_data("social.json"))
 data.update(add_data("education.json"))
 data.update(add_data("experience.json"))
 data.update(add_data("references.json"))
+data.update(add_data("interests.json"))
 
 # Set up Jinja environment
 env = Environment(loader=FileSystemLoader('templates'))
